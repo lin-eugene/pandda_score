42bbaaaaaba6b70d0f5205a579df08ece5e8205d
lin-eugene <lin_eugene@icloud.com>
2022-06-09 16:40:49 +0100

    modified removeligdensity.py and test.py

--- removeligdensity.py
+++ removeligdensity.py
@@ -1,48 +1,43 @@
 import numpy as np
 import gemmi
 import matplotlib.pyplot as plt
+import pathlib
+import shutil
 
-def remove_density(x,y,z,map):
-    """
-    Masks out electron density at specified Cartesian coordinates
-    x,y,z — coordinates
-    map — gemmi map class
-    """
-    coord = gemmi.Position(x, y, z)
-    map.grid.set_points_around(coord,radius=1,value=0)
+def rmligdensity(filename_structure, filename_map, ccp4_out_name):
+    structure = gemmi.read_structure(filename_structure)
+    map = gemmi.read_ccp4_map(filename_map,setup=True)
+
+    #initialising list of positions and vdw radii
+    positions = []
+    radii = []
+
+    #appending ligand positions/coordinates and vdw radii into lists
+    for model in structure:
+        for chain in model:
+            for residue in chain:
+                if residue.name == 'LIG':
+                    for atom in residue:
+                        positions.append(atom.pos)
+                        radii.append(atom.element.vdw_r)
+
+
+    #removing ligand electron density
+    for i in range(len(positions)):
+        map.grid.set_points_around(positions[i],radius=radii[i],value=0)
+
+    #symmetrising map
     map.grid.symmetrize_min
+
+    #writing output map
+    ccp4_out = map
+    ccp4_out.write_ccp4_map(ccp4_out_name)
+
 
 filename_structure = 'data/Namdinator test files/MURE/MUREECA-x0244good/MUREECA-x0244-ligandbuilt.pdb'
 filename_map = 'data/Namdinator test files/MURE/MUREECA-x0244good/Remodelling results/MUREECA-x0244-aligned-event_1.ccp4'
+ccp4_out_name = 'out1.ccp4'
 
-structure = gemmi.read_structure(filename_structure)
-map = gemmi.read_ccp4_map(filename_map,setup=True)
-# print(structure[0])
-# print(structure[0][0])
-# print(structure[0][0][0])
-# print(structure[0][0][0][0].pos)
-# print(structure[0][0][0][0].element.vdw_r)
-# print(dir(structure[0][0][0][0]))
-# print(dir(structure))
+rmligdensity(filename_structure, filename_map, ccp4_out_name)
 
 
-positions = []
-radii = []
-
-
-for model in structure:
-    for chain in model:
-        for residue in chain:
-            if residue.name == 'LIG':
-                for atom in residue:
-                    positions.append(atom.pos)
-                    radii.append(atom.element.vdw_r)
-
-
-for i in range(len(positions)):
-    map.grid.set_points_around(positions[i],radius=radii[i],value=0)
-
-map.grid.symmetrize_min
-
-ccp4_out = map
-ccp4_out.write_ccp4_map('out1.ccp4')
