b8d87fd6ed012bcfdf902a9918b91d3d363b2ff6
lin-eugene <lin_eugene@icloud.com>
2022-07-08 14:52:02 +0100

    added files

--- fileprep.py
+++ fileprep.py
@@ -2,9 +2,10 @@ import pandas as pd
 import pathlib
 
 
-def log_built_ligands(path_pandda_root):
+def log_built_ligands(path_analyses_dir):
+    path_panddas = path_analyses_dir / 'panddas'
     
-    path_analyses = [x for x in path_pandda_root.iterdir() if x.is_dir() and 'analyses' in x.stem]
+    path_analyses = [x for x in path_panddas.iterdir() if x.is_dir() and 'analyses' in x.stem]
     path_events_csv = path_analyses[0] / 'pandda_inspect_events.csv'
 
     events_csv = pd.read_csv(path_events_csv)
@@ -13,7 +14,7 @@ def log_built_ligands(path_pandda_root):
     ligand_built = events_csv.loc[(events_csv['Ligand Placed']==True) & (events_csv['Ligand Confidence']=='High')]
 
     #extracting and saving relevant columns
-    ligand_built = ligand_built[['dtag','event_idx', 'x', 'y', 'z', '1-BDC', 'high_resolution','Ligand Placed']]
+    ligand_built = ligand_built[['dtag','event_idx', 'x', 'y', 'z', '1-BDC', 'high_resolution','Ligand Placed', 'Ligand Confidence']]
 
     #saving pandas dataframe into csv
     outfile = path_analyses[0] / 'pandda_builtligands.csv'
@@ -22,16 +23,20 @@ def log_built_ligands(path_pandda_root):
     return ligand_built
 
 
-def log_training_data_paths(path_pandda_root):
+def log_training_data_paths(path_analyses_dir):
 
-    path_analyses = [x for x in path_pandda_root.iterdir() if x.is_dir() and 'analyses' in x.stem]
-    path_proc_datasets = path_pandda_root / 'processed_datasets'
+    path_panddas = path_analyses_dir / 'panddas'
+    path_analyses = [x for x in path_panddas.iterdir() if x.is_dir() and 'analyses' in x.stem]
+    path_proc_datasets = path_panddas / 'processed_datasets'
     path_ligand_csv = path_analyses[0] / 'pandda_builtligands.csv'
-    initial_model_path = path_proc_datasets.parent.parent / 'initial_model'
-    if initial_model_path.is_dir():
+    path_initial_model = path_proc_datasets.parent.parent / 'initial_model'
+    if path_initial_model.is_dir():
         print('initial_model directory exists')
-    elif initial_model_path.is_dir() == False:
-        initial_model_path = path_proc_datasets.parent.parent / 'model_building'
+    elif path_initial_model.is_dir() == False:
+        path_initial_model = path_analyses / 'model_building'
+
+        if path_initial_model.is_dir() == False:
+            print('no built models')
 
     if path_ligand_csv.is_file():
         ligand_csv = pd.read_csv(path_ligand_csv)
@@ -48,7 +53,7 @@ def log_training_data_paths(path_pandda_root):
         data_paths['dataset'].append(row.dtag) #add dataset name
 
         #add event map path
-        event_map_path = path_proc_datasets / f'{row.dtag}' / f'{row.dtag}-event_{row.event_idx}_1-BDC_{row._7}_map.native.ccp4'
+        event_map_path = path_proc_datasets / f'{row.dtag}' / f'{row.dtag}-event_{row.event_idx}_1-BDC_{row._7}_map.native.ccp4' # only selecting relevant event map from which ligand was built
         if event_map_path.is_file():
             data_paths['event_map'].append(event_map_path)
         else:
@@ -69,7 +74,7 @@ def log_training_data_paths(path_pandda_root):
             data_paths['input_model'].append('')
 
         #add output model path
-        output_path = initial_model_path / f'{row.dtag}' / f'{row.dtag}-pandda-model.pdb'
+        output_path = path_initial_model / f'{row.dtag}' / f'{row.dtag}-pandda-model.pdb'
         if output_path.is_file():
             data_paths['output_model'].append(output_path)
         else:
@@ -82,7 +87,7 @@ def log_training_data_paths(path_pandda_root):
     return pd_datapaths
 
 if __name__ == "__main__":
-    path_pandda_root = pathlib.Path.cwd() / 'data' / 'testdirs' / 'MURE' / 'panddas'
-    ligand_built = log_built_ligands(path_pandda_root)
-    pd_datapaths = log_training_data_paths(path_pandda_root)
+    path_analyses_dir = pathlib.Path.cwd() / 'data' / 'testdirs' / 'MID2A' / 'processing' / 'analysis'
+    ligand_built = log_built_ligands(path_analyses_dir)
+    #pd_datapaths = log_training_data_paths(path_analyses_dir)
 
--- removeligdensity.py
+++ removeligdensity.py
@@ -1,9 +1,7 @@
 import numpy as np
 import gemmi
-<<<<<<< HEAD
 import os
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
 from pathlib import Path
 import shutil
 
@@ -37,85 +35,19 @@ def rmligdensity(filename_structure: str, filename_map: str, ccp4_out_name: str)
 
     #symmetrising map
     map.grid.symmetrize_min
-=======
-=======
-import matplotlib.pyplot as plt
->>>>>>> 42bbaaaaaba6b70d0f5205a579df08ece5e8205d
-import pathlib
-import shutil
-
-def rmligdensity(filename_structure, filename_map, ccp4_out_name):
-    structure = gemmi.read_structure(filename_structure)
-    map = gemmi.read_ccp4_map(filename_map,setup=True)
-
-    #initialising list of positions and vdw radii
-    positions = []
-    radii = []
-
-    #appending ligand positions/coordinates and vdw radii into lists
-    for model in structure:
-        for chain in model:
-            for residue in chain:
-                if residue.name == 'LIG':
-                    for atom in residue:
-                        positions.append(atom.pos)
-                        radii.append(atom.element.vdw_r)
-<<<<<<< HEAD
->>>>>>> modified removeligdensity.py and test.py
 
     #writing output map
     ccp4_out = map
     ccp4_out.write_ccp4_map(ccp4_out_name)
 
-<<<<<<< HEAD
-=======
-    #removing ligand electron density
-    for i in range(len(positions)):
-        map.grid.set_points_around(positions[i],radius=radii[i],value=0)
->>>>>>> modified removeligdensity.py and test.py
-=======
 
-
-    #removing ligand electron density
-    for i in range(len(positions)):
-        map.grid.set_points_around(positions[i],radius=radii[i],value=0)
->>>>>>> 42bbaaaaaba6b70d0f5205a579df08ece5e8205d
-
-    #symmetrising map
-    map.grid.symmetrize_min
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 ###########
-=======
-    #writing output map
-    ccp4_out = map
-    ccp4_out.write_ccp4_map(ccp4_out_name)
->>>>>>> modified removeligdensity.py and test.py
 
 if __name__ == "__main__":
     filename_structure = 'data/Namdinator test files/MURE/MUREECA-x0244good/MUREECA-x0244-ligandbuilt.pdb'
     filename_map = 'data/Namdinator test files/MURE/MUREECA-x0244good/Remodelling results/MUREECA-x0244-aligned-event_1.ccp4'
     ccp4_out_name = 'data/Namdinator test files/MURE/MUREECA-x0244good/out1.ccp4'
 
-<<<<<<< HEAD
     rmligdensity(filename_structure, filename_map, ccp4_out_name)
-=======
-=======
-    #writing output map
-    ccp4_out = map
-    ccp4_out.write_ccp4_map(ccp4_out_name)
-
-
->>>>>>> 42bbaaaaaba6b70d0f5205a579df08ece5e8205d
-filename_structure = 'data/Namdinator test files/MURE/MUREECA-x0244good/MUREECA-x0244-ligandbuilt.pdb'
-filename_map = 'data/Namdinator test files/MURE/MUREECA-x0244good/Remodelling results/MUREECA-x0244-aligned-event_1.ccp4'
-ccp4_out_name = 'out1.ccp4'
-
-rmligdensity(filename_structure, filename_map, ccp4_out_name)
-<<<<<<< HEAD
->>>>>>> modified removeligdensity.py and test.py
-=======
->>>>>>> 42bbaaaaaba6b70d0f5205a579df08ece5e8205d
 
 
