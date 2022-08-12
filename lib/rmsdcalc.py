1c81fe37d8c36344e66ac2394eccc96a0b8c2c30
lin-eugene <lin_eugene@icloud.com>
2022-08-12 15:18:36 +0100

    modified fileprep.py

--- lib/rmsdcalc.py
+++ lib/rmsdcalc.py
@@ -98,7 +98,9 @@ def calc_rmsds(input_pdb_name: pathlib.PosixPath, remodelled_pdb_name: pathlib.P
         # # print(polymer2)
         # polymer2 = superpose(polymer1, polymer2)
         dist_diff = calc_dist_diff(pair[0], pair[1])
-        rmsd_dict[pair[0].name] = dist_diff
+
+        if not bool(dist_diff): #if dist_diff dictionary is not empty
+            rmsd_dict[pair[0].name] = dist_diff
 
     return rmsd_dict, chain_mapping
 
