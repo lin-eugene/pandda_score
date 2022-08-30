7c2fbe2ba10a6b1956e18df463a2a9e00f301470
lin-eugene <lin_eugene@icloud.com>
2022-08-30 17:17:23 +0100

    updated nonstandard_paths.csv

--- fileprep.py
+++ fileprep.py
@@ -382,11 +382,11 @@ def find_contacts(df_residues):
             residue_idx = contact[1]
             res_name = contact[2]
             contact_row = df_residues.loc[
-                df_residues['remodelled']==False and
-                df_residues['input_model']==row.input_model and
-                df_residues['output_chain_idx']==chain_idx and
-                df_residues['residue_output_idx']==residue_idx and
-                df_residues['residue_name']==res_name
+                (df_residues['remodelled']==False) and
+                (df_residues['input_model']==row.input_model) and
+                (df_residues['output_chain_idx']==chain_idx) and
+                (df_residues['residue_output_idx']==residue_idx) and
+                (df_residues['residue_name']==res_name)
                 ]
             df_negative_data = pd.concat(df_negative_data, contact_row)
     
