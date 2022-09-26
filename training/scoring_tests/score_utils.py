da4cf025a568db4182b9fa28d7133496b81399e4
lin-eugene <lin_eugene@icloud.com>
2022-09-26 11:43:36 +0100

    added files

--- training/scoring_tests/score_utils.py
+++ training/scoring_tests/score_utils.py
@@ -7,7 +7,8 @@ def save_output(output_labels: List[Dict],
                 pandda_model_path: str):
     df = pd.DataFrame(output_labels)
     output_dir = pathlib.Path(pandda_model_path).resolve().parent
-    output_file = output_dir / 'output_labels.csv'
+    pandda_model_name = pathlib.Path(pandda_model_path).resolve().stem
+    output_file = output_dir / f'output_labels_{pandda_model_name}.csv'
 
     print(f'saving output labels to csv as: {output_file}..')
     df.to_csv(output_file, index=False)
