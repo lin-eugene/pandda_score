1649a92a8f845730083e0b5756be7073b5a2ef3a
lin-eugene <lin_eugene@icloud.com>
2022-09-28 14:13:25 +0100

    modified files

--- training/scoring_tests/score_engine.py
+++ training/scoring_tests/score_engine.py
@@ -9,6 +9,7 @@ def get_labels_per_residue(event_map: str,
                             input_chain_idx: int,
                             input_residue_idx: int,
                             residue_name: str,
+                            pred_probability: float,
                             pred_label: int):
 
     return {
@@ -17,12 +18,14 @@ def get_labels_per_residue(event_map: str,
         'input_chain_idx': input_chain_idx,
         'input_residue_idx': input_residue_idx,
         'residue_name': residue_name,
+        'pred_probabilities': pred_probability,
         'pred_label': pred_label,
 
     }
 
 
 def record_labels_per_batch(batch: Dict[str, Any],
+                                    test_pred_probability: torch.Tensor,
                                     test_pred_labels: torch.Tensor,):
     
     event_map_name = batch['event_map_name']
@@ -30,6 +33,7 @@ def record_labels_per_batch(batch: Dict[str, Any],
     input_chain_idx = batch['input_chain_idx'].tolist()
     input_residue_idx = batch['input_residue_idx'].tolist()
     residue_name = batch['residue_name']
+    pred_probability = test_pred_probability.tolist()
     pred_label = test_pred_labels.tolist()
 
     return list(map(get_labels_per_residue, 
@@ -38,6 +42,7 @@ def record_labels_per_batch(batch: Dict[str, Any],
                     input_chain_idx,
                     input_residue_idx,
                     residue_name,
+                    pred_probability,
                     pred_label))
 
 def scoring_loop(model: torch.nn.Module,
@@ -68,12 +73,13 @@ def scoring_loop(model: torch.nn.Module,
 
 
             if debug_pred_logits.size(dim=1) > 1:
+                debug_pred_probability = torch.softmax(debug_pred_logits, dim=1)[:,1]
                 debug_pred_labels = torch.argmax(torch.softmax(debug_pred_logits, dim=1), dim=1) #for multiclass classification
 
             else:
                 debug_pred_labels = torch.round(torch.sigmoid(debug_pred_logits)) #for binary classification
 
             logging.debug(sample)
-            output_labels += record_labels_per_batch(sample, debug_pred_labels)
+            output_labels += record_labels_per_batch(sample, debug_pred_probability, debug_pred_labels)
 
     return output_labels
